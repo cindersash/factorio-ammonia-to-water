e99a11eca15ae5c3442b15df428b4d428a7b56f1
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2025-04-05 15:08:42 -0500

    Fixes

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,4 @@
-local function allow_recipe_productivity(recipe_name)
-	for _, module in pairs(data.raw.module) do
-		if module.category == "productivity" and module.limitation then
-			table.insert(module.limitation, recipe_name)
-		end
-	end
-end
-
-allow_recipe_productivity("aw_ammonia-to-ice")
+table.insert(data.raw["technology"]["planet-discovery-aquilo"].effects, {
+  type = "unlock-recipe",
+  recipe = "aw_ammonia-to-ice"
+})
--- data.lua
+++ data.lua
@@ -3,7 +3,7 @@ local added_recipes = {}
 local ammonia_to_ice_recipe = {
   type = "recipe",
   name = "aw_ammonia-to-ice",
-  icon = "__ammonia-to-water__/graphics/ammonia-to-ice.png",
+  icon = "__ammonia-to-water__/graphics/ammonia-to-ice-recipe.png",
   icon_size = 64,
   crafting_machine_tint = {
     primary = { 21, 28, 171 },          -- primary color 1
@@ -17,24 +17,12 @@ local ammonia_to_ice_recipe = {
   results = {
     { type = "item", name = "ice", amount = 5 },
   },
-  category = "chemistry",
+  category = "chemistry-or-cryogenics",
+  enabled = false,
+  allow_productivity = true,
   subgroup = "fluid-recipes",
   energy_required = 1,
   allow_decomposition = false
 }
-table.insert(added_recipes, { recipe = ammonia_to_ice_recipe, tech = "planet-discovery-aquilo" })
-table.insert(added_recipes, { recipe = data.raw["recipe"]["chemical-plant"], tech = "planet-discovery-aquilo" })
-data:extend { ammonia_to_ice_recipe }
 
--- Add recipe unlocks
-local tech = data.raw["technology"]
-for _, to_add in ipairs(added_recipes) do
-  if (tech[to_add.tech])
-  then
-    to_add.recipe.enabled = false
-    table.insert(tech[to_add.tech].effects, {
-      type = "unlock-recipe",
-      recipe = to_add.recipe.name
-    })
-  end
-end
+data:extend { ammonia_to_ice_recipe }
